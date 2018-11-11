aa5ddad9cdf784365f09867b4405d764466dd830
Vistassja <44507941+Vistassja@users.noreply.github.com>
2018-11-10 23:09:26 -0400

    Add files via upload

--- MonthYearPanel.java
+++ MonthYearPanel.java
@@ -11,7 +11,6 @@ import javax.swing.*;
 public class MonthYearPanel extends JPanel {
 	public MonthYearPanel()	{
 		OneWeek week = new OneWeek();
-
 		Font font = new Font("Courier", Font.PLAIN, 40);   
 		String total_label = new String();
 		String month = new String();
@@ -25,13 +24,23 @@ public class MonthYearPanel extends JPanel {
 		else
 			month = week.get_month(0);
 
-
 		if (week.get_year(0) != week.get_year(6))	// If the week stretches over two years
 			year = Integer.toString(week.get_year(0)) + " / " + 
 					Integer.toString(week.get_year(6));
 		else
 			year = Integer.toString(week.get_year(0));
+		
 		total_label = month + " " + year;
+		
+		// help button
+		ImageIcon help_icon = new ImageIcon("icons/help_icon.png");
+		String help_text = new String("<html>Select times in the drop down menus corresponding to "
+				+"when you are <strong>unavailable</strong>.<br>When finished, click Calculate.</html>");
+		JLabel help = new JLabel(help_icon);
+		help.setToolTipText(help_text);
+		help.setPreferredSize(getPreferredSize());
+		add(help);
+		help.setBounds(940, 10, 32, 32);
 
 		setLayout(new BorderLayout());
 		setBackground(panelcol);
